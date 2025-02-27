825ca9dfe10fcf08f91c6c8f6e2765d0c83de674
Elina-ml <elton31542@gmail.com>
2025-02-27 06:13:14 +0300

    Добавлены задания по уроку 2

--- 02_lesson/lesson_2_task_3.py
+++
@@ -1,4 +0,0 @@
-square = ['a', 'b', 'c', 'd']
-square1 = int(input("Укажите длину сторон a и c: "))
-square2 = int(input("Укажите длину сторон b и d: "))
-print("Площадь квадрата = ", square1*square2)
